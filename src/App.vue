01c1fb5aacdb88de651c7d1135237ffb590c4d2e
MAGIN Alexcis <Alexcis.MAGIN@cegedim.com>
2018-07-20 13:00:25 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-app id="app" dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-tile v-for="elm in menu" :key="'m_'+elm.name" @click="goTo(elm.name)">
+          <v-list-tile-action>
+            <v-icon>{{elm.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{elm.label}}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <v-footer app fixed>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+const menu = [
+  {name: 'home', icon: 'home', label: 'Home'},
+  {name: 'about', icon: 'help_outline', label: 'A propos'},
+  {name: 'config', icon: 'settings', label: 'Configs'}
+]
+export default {
+  name: 'App',
+  data () {
+    return {
+      drawer: false,
+      menu: menu
+    }
+  },
+  methods: {
+    goTo (item, event) {
+      console.log(item, event)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import colors from 'vuetify/es5/util/colors'
+
+Vue.config.productionTip = false
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.indigo.base,
+    secondary: colors.grey.darken1,
+    accent: colors.shades.black,
+    error: colors.red.accent3
+  }
+})
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
