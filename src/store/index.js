819390bb7d1ff4a9df3b7386ebba80509c95033d
MAGIN Alexcis <Alexcis.MAGIN@cegedim.com>
2018-11-15 11:49:30 +0100

    crecction erreur

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,6 @@ import matieres from './matieres'
 import years from './years'
 import user from './user'
 import createPersistedState from 'vuex-persistedstate'
-import firebase from 'firebase'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -18,16 +17,16 @@ export default new Vuex.Store({
       console.log('test')
     },
     userInfos ({state}) {
-      let userId = firebase.auth().currentUser.uid
+      // let userId = firebase.auth().currentUser.uid
       let email = state.user.user.email
       // eslint-disable-next-line
-      db.collection("users").doc(userId).set({
+      db.collection("users").doc(email).set({
         username: name,
         laemailst: email,
         profile_picture: '1815'
       })
-        .then(function (docRef) {
-          console.log('Document written with ID: ', docRef.id)
+        .then(function () {
+          console.log('Document written')
         })
         .catch(function (error) {
           console.error('Error adding document: ', error)
